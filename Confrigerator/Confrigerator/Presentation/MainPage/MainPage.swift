f125f4fd37514aa4acc23ce82107e439ceab501d
BAE <lexon1209@gmail.com>
2024-05-01 22:12:35 +0900

    [#1] DESIGN: 로그인 페이지 레이아웃

---
+++ Confrigerator/Confrigerator/Presentation/MainPage/MainPage.swift
@@ -0,0 +1,18 @@
+//
+//  MainPage.swift
+//  Confrigerator
+//
+//  Created by BAE on 5/1/24.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPage()
+}
