075859fc5cee21e9dc77267c8080d842358b5c0b
BAE <lexon1209@gmail.com>
2024-10-24 01:28:44 +0900

    feat: 기프티콘 추가 뷰 구현

--- Confrigerator/Confrigerator/Presentation/MainPage/Tabs/AddConTab/View/AddConTab.swift
+++ Confrigerator/Confrigerator/Presentation/MainPage/Tabs/AddConTab/View/AddConTab.swift
@@ -6,13 +6,119 @@
 //
 
 import SwiftUI
+import PopupView
 
 struct AddConTab: View {
+    @State private var selectedStore: String = "판매처를 선택해주세요."
+    @State private var serialNumber: String = ""
+    @State private var showToast: Bool = false // 토스트 메시지를 제어하는 상태 변수
+    
     var body: some View {
-        Text("Add GiftCon Tab")
+        NavigationView {
+            VStack {
+                List {
+                    Section(header: Text("판매처")) {
+                        Menu {
+                            Button(action: {
+                                selectedStore = "CU"
+                            }) {
+                                Label("CU", systemImage: "bag")
+                            }
+                            Button(action: {
+                                selectedStore = "GS25"
+                            }) {
+                                Label("GS25", systemImage: "cart")
+                            }
+                            Button(action: {
+                                selectedStore = "세븐일레븐"
+                            }) {
+                                Label("세븐일레븐", systemImage: "building")
+                            }
+                            Button(action: {
+                                selectedStore = "미니스톱"
+                            }) {
+                                Label("미니스톱", systemImage: "storefront")
+                            }
+                        } label: {
+                            Text(selectedStore)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.vertical, 4)
+                        }
+                    }
+                    
+                    Section(header: Text("일련번호")) {
+                        HStack {
+                            TextField("일련번호를 입력해주세요.", text: $serialNumber)
+                                .padding(.vertical, 4)
+                            Spacer()
+                            
+                            Text("조회")
+                                .foregroundColor(Color.accentColor)
+                                .padding(.vertical, 4)
+                                .padding(.horizontal)
+                                .highPriorityGesture(
+                                    TapGesture().onEnded {
+                                        print("일련번호 : \(serialNumber)")
+                                    }
+                                )
+                        }
+                    }
+                }
+                
+                
+                Spacer()
+                
+                Button {
+                    print("판매처: \(selectedStore), 일련번호: \(serialNumber)")
+                    showToast.toggle()
+                    selectedStore = "판매처를 선택해주세요."
+                    serialNumber = ""
+                } label: {
+                    Text("추가하기")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.accentColor)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .padding(.horizontal)
+                .padding(.bottom, 32)
+            }
+            .popup(isPresented: $showToast, view: {
+                VStack {
+                    Spacer()
+                    Text("기프티콘이 추가되었습니다.")
+                        .font(.title3)
+                        .bold()
+                        .padding(.bottom, 20)
+                }
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.18)
+                .background(.ultraThinMaterial)
+                
+            }, customize: {
+                $0
+                    .type(.toast)
+                    .position(.top)
+                    .closeOnTapOutside(true)
+                    .isOpaque(true)
+                    .autohideIn(2)
+            })
+            .navigationTitle("기프티콘 추가")
+            .onTapGesture {
+                UIApplication.shared.endEditing() // 다른 영역을 터치했을 때 키보드 숨김
+                print("touch")
+            }
+        }
+    }
+}
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
 #Preview {
     AddConTab()
 }
+
--- Confrigerator/Confrigerator/Presentation/MainPage/View/MainPage.swift
+++ Confrigerator/Confrigerator/Presentation/MainPage/View/MainPage.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct MainPage: View {
     private let images = Array(repeating: "photo", count: 6)
@@ -37,3 +38,32 @@ struct MainPage: View {
 #Preview {
     MainPage()
 }
+
+//
+//class CustomSubscrbier: Subscriber {
+//
+//    func receive(completion: Subscribers.Completion<Never>) {
+//        print("모든 데이터의 발행이 완료되었습니다.")
+//    }
+//
+//    func receive(subscription: Subscription) {
+//        print("데이터의 구독을 시작합니다.")
+//        //구독할 데이터의 개수를 제한하지않습니다.
+//        subscription.request(.unlimited)
+//    }
+//
+//    func receive(_ input: String) -> Subscribers.Demand {
+//        print("데이터를 받았습니다.", input)
+//        return .none
+//    }
+//    
+//    typealias Input = String //성공타입
+//    typealias Failure = Never //실패타입
+//    
+//}
+//
+//let publisher = ["A","B","C","D","E","F","G"].publisher
+//
+//let subscriber = CustomSubscrbier()
+//
+//publisher.subscribe(subscriber)
